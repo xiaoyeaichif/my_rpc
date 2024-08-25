1d5211bba158f0a71fbe4737a0a5df016f24a59c
xiaoyeaichif <1125035561@qq.com>
2024-08-25 22:58:09 +0800

    消费者修改

--- example/caller/calluserservice.cc
+++ example/caller/calluserservice.cc
@@ -1,9 +1,36 @@
 #include <iostream>
 #include "mprpcapplication.h"
-#include "user.pb.h"yejie520
+#include "user.pb.h"
+#include "mprpcchannel.h"
 
-
-int main(int agrv,char ** argc)
+int main(int argc, char **argv)
 {
+    // 整个程序启动以后，想使用mprpc框架来享受rpc服务调用，一定需要先调用框架的初始化函数（只初始化一次）
+    MprpcApplication::Init(argc, argv);
 
+    // 演示调用远程发布的rpc方法Login
+    fixbug::UserServiceRpc_Stub stub(new MprpcChannel());
+    // rpc方法的请求参数
+    fixbug::LoginRequest request;
+    request.set_name("zhang san");
+    request.set_pwd("123456");
+    // rpc方法的响应
+    fixbug::LoginResponse response;
+    // 发起rpc方法的调用  同步的rpc调用过程  MprpcChannel::callmethod
+    //同步阻塞的方式
+    stub.Login(nullptr, &request, &response, nullptr); // RpcChannel->RpcChannel::callMethod 集中来做所有rpc方法调用的参数序列化和网络发送
+
+    // 一次rpc调用完成，读调用的结果
+    if (0 == response.result().errorcode())
+    {
+        //调用正确
+        std::cout << "rpc login response success:" << response.success() << std::endl;
+    }
+    else
+    {
+        //调用失败
+        std::cout << "rpc login response error : " << response.result().errormsg() << std::endl;
+    }
+
+    return 0;
 }
