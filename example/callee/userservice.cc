e0c0e1564074a7159939ab4dc8f34e8d9975c38f
xiaoyeaichif <1125035561@qq.com>
2024-08-25 09:54:09 +0800

    初始化服务和构建cmake文件

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include "user.pb.h"
+#include "mprpcapplication.h"
+#include "rpcprovider.h"
 
 
 
@@ -50,14 +52,15 @@ int main(int argc,char **argv)
 {
     // 调用框架的初始化操作
     // 静态函数
-    // MprpcApplication::Init(argc, argv);
+    MprpcApplication::Init(argc, argv);
 
-    // // provider是一个rpc网络服务对象。把UserService对象发布到rpc节点上
-    // RpcProvider provider;
-    // provider.NotifyService(new UserService());
+    // provider是一个rpc网络服务对象。把UserService对象发布到rpc节点上
+    RpcProvider provider;
+    provider.NotifyService(new UserService());
 
-    // // 启动一个rpc服务发布节点   Run以后，进程进入阻塞状态，等待远程的rpc调用请求
-    // provider.Run();
+    // 启动一个rpc服务发布节点   Run以后，进程进入阻塞状态，等待远程的rpc调用请求
+    // 是一个同步过程
+    provider.Run();
 
     return 0;
 }
