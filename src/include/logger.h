bbbd3dc4017300fdd5a9aaa196e566891b2956d2
xiaoyeaichif <1125035561@qq.com>
2024-08-26 01:30:50 +0800

    日志类的构建

--- src/include/logger.h
+++ src/include/logger.h
@@ -1,4 +1,31 @@
 #pragma once
+#include "lockqueue.h"
+#include <string>
+
+
+// 定义宏 LOG_INFO("xxx %d %s", 20, "xxxx")
+// 可变参的日志宏
+#define LOG_INFO(logmsgformat, ...) \
+    do \
+    {  \
+        Logger &logger = Logger::GetInstance(); \
+        logger.SetLogLevel(INFO); \
+        char c[1024] = {0}; \
+        snprintf(c, 1024, logmsgformat, ##__VA_ARGS__); \
+        logger.Log(c); \
+    } while(0) \
+
+#define LOG_ERR(logmsgformat, ...) \
+    do \
+    {  \
+        Logger &logger = Logger::GetInstance(); \
+        logger.SetLogLevel(ERROR); \
+        char c[1024] = {0}; \
+        snprintf(c, 1024, logmsgformat, ##__VA_ARGS__); \
+        logger.Log(c); \
+    } while(0) \
+
+
 
 // 框架的日志系统
 
@@ -14,5 +41,22 @@ enum Loglevel
 // 使用单例模式
 class Logger
 {
-
+public:
+    // 获取日志的单例
+    static Logger& GetInstance();
+    // 设置日志级别 
+    void SetLogLevel(LogLevel level);
+    // 写日志
+    void Log(std::string &msg);
+private:
+    // 设置日志的级别
+    int m_loglevel;
+    LockQueue<std::string>m_lckQue; // 日志缓冲队列
+    // 
+    Logger();
+    // 禁止拷贝构造函数，移动构造函数以及赋值函数
+    Logger(const Logger &) = delete;
+    Logger(const Logger &&) = delete;
+    Logger &operator=(const Logger &) = delete;
+    Logger &operator=(Logger&&) = delete;
 };
---
+++ src/logger.cc
@@ -0,0 +1,76 @@
+#include "logger.h"
+#include <time.h>
+#include <iostream>
+
+
+ // 获取日志的单例
+Logger& Logger::GetInstance()
+{
+    static Logger logger;
+    return logger;
+}
+// 设置日志级别 
+void Logger::SetLogLevel(LogLevel level)
+{
+    m_loglevel = level;
+}
+// 写日志， 把日志信息写入lockqueue缓冲区当中
+void Logger::Log(std::string &msg)
+{
+    m_lckQue.Push(msg);
+}
+
+// 默认构造函数的实现
+Logger::Logger()
+{
+    // 启动专门的写日志线程
+    std::thread writeLogTask(
+        // [&] 捕获全部变量以引用的方式
+        // 形参列表为空
+        // 返回值为void
+        [&]()
+        {
+            // 后台线程一直将日志写进队列中
+            while(true)
+            {
+                // 获取当前的日期
+                time_t now = time(nullptr);
+                // 具体的时分秒都在tm这个结构体中
+                tm *nowtm = localtime(&now);
+
+                char file_name[128]; // 缓冲区大小
+                // 格式化字符串
+                // 只有具体的年月日
+                sprintf(file_name, "%d-%d-%d-log.txt", nowtm->tm_year+1900, nowtm->tm_mon+1, nowtm->tm_mday);
+
+                FILE *pf = fopen(file_name, "a+");
+                if (pf == nullptr)
+                {
+                    std::cout << "logger file : " << file_name << " open error!" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+
+                // 获取队头的日志信息
+                std::string msg = m_lckQue.Pop();
+
+                // 给年月日加上时分秒
+                // 并且判断日志的等级
+                char time_buf[128] = {0};
+                sprintf(time_buf, "%d:%d:%d =>[%s] ", 
+                        nowtm->tm_hour, 
+                        nowtm->tm_min, 
+                        nowtm->tm_sec,
+                        (m_loglevel == INFO ? "info" : "error"));
+                msg.insert(0, time_buf);
+                msg.append("\n");
+
+                // 将组合时分秒的数据日志放在日志文件的一行中
+                fputs(msg.c_str(), pf);
+                // 关闭文件
+                fclose(pf);
+            }
+        }
+    );
+    // 与主线程分离,后台线程一直执行
+    writeLogTask.detach();
+}
