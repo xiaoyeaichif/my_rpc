f513c82bbd7f909d15682660ebca64977ddd91f7
xiaoyeaichif <1125035561@qq.com>
2024-08-26 00:42:49 +0800

    增加线程安全的队列

---
+++ src/include/lockqueue.h
@@ -0,0 +1,52 @@
+#pragma once
+#include<queue>
+#include<mutex>
+#include<condition_variable>
+#include<thread>
+
+
+// 异步写日志的日志队列
+// 注意的，当队列为空，不能写日志，一定要不为空才能写
+template<typename T>
+class LockQueue
+{
+public:
+    //  向队列添加元素
+    void Push(const T & data)
+    {
+        // 先抢到锁的人先插入元素
+        // 或者使用lock_guard
+        std::unique_lock<std::mutex>lock(m_mutex);
+        m_queue.push(data);
+        // 提醒消费者也就是这里的Pop函数消费队列中的数据
+        // 由于我们只使用一个线程进行写事件，所以只需要唤醒一个写线程就可以
+        m_cond_variable.notify_one();
+    }
+    T Pop()
+    {
+        // 循环检测队列是否为空
+        // 如果队列为空，此时不能消费队列中的元素
+        // 所以需要通过条件变量提醒Push往线程中加数据
+        // 队列在这里为临界资源
+
+        // 将元素弹出时，必须先获取锁
+        std::unique_lock<std::mutex>_lock(m_mutex);
+        while(m_queue.empty())
+        {
+            // 队列为空
+            // 不能弹出元素，所以需要释放锁资源
+            m_cond_variable.wait(_lock);
+        }
+
+        // 队列元素不为空，可以弹出元素以及获取队列的头部元素
+        T data = m_queue.front();
+        // 弹出元素
+        m_queue.pop();
+        return data;
+    }
+    
+private:
+    std::queue<T>m_queue;
+    std::mutex m_mutex;
+    std::condition_variable m_cond_variable;
+};
---
+++ src/include/logger.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// 框架的日志系统
+
+// 日志级别
+enum Loglevel
+{
+    INFO; // 普通信息
+    ERROR; // 错误信息
+}
+
+
+
+// 使用单例模式
+class Logger
+{
+
+};
